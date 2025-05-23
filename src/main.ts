422805074b7ff0f1551e942a36872f752b751617
Juan Jose Jaramillo <juan.jaramillo14@eia.edu.co>
2025-05-23 13:02:20 -0500

    Se corrigen arreglos en el main para realizar conexion con el front-end

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,15 @@ console.log('DATABASE_URL desde dotenv:', process.env.DATABASE_URL);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS para el frontend en http://localhost:5173
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
 
